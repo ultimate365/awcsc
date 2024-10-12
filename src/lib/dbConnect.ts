bfc143477a15a423071eaec995065f46c08345b1
Ultimate 365 <maidul365@gmail.com>
2024-10-12 22:03:29 +0530

    first commit

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -12,7 +12,11 @@ async function dbConnect(): Promise<void> {
     return;
   }
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+    const db = await mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb://amtawestsports:aredyGbRMtYVgFlR@ac-qh7ppgr-shard-00-00.8glfa0t.mongodb.net:27017,ac-qh7ppgr-shard-00-01.8glfa0t.mongodb.net:27017,ac-qh7ppgr-shard-00-02.8glfa0t.mongodb.net:27017/awcsc?ssl=true&replicaSet=atlas-ailzos-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0",
+      {}
+    );
     connection.isConnected = db.connections[0].readyState;
     console.log("Connected to Database");
   } catch (error) {
