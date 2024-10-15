b722e9f78ef10ce17b119eb13a1c0f14362479bb
Ultimate 365 <maidul365@gmail.com>
2024-10-15 21:51:20 +0530

    Mailer Updated

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -11,28 +11,70 @@ const sendEmail = async ({ email, code, name }: any) => {
       },
     });
 
-    const mailOptions = {
-      from: mail,
+    // const mailOptions = {
+    //   from: mail,
+    //   to: email,
+    //   subject: `Reset your Password: Mail no ${Math.floor(
+    //     Math.random() * 1000 + 1
+    //   )}`,
+    //   // text: `Your OTP is ${otp}`,
+    //   html: `<h1 style="text-align:center; color:blue; ">Hello Dear ${name}!</h1>
+    //     <h2 style="text-align:center; color:blue;">Your OTP is ${code}. Please use this OTP to reset your password.</h2>`,
+    // };
+    // const mailResponse = await transport.sendMail(
+    //   mailOptions,
+    //   function (error: any, info: any) {
+    //     if (error) {
+    //       console.log("error", error);
+    //     } else {
+    //       console.log("Email Sent: " + info.response);
+    //     }
+    //   }
+    // );
+
+    // return mailResponse;
+
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transport.verify(function (error: any, success: any) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
+    });
+
+    const mailData = {
+      from: {
+        name: `WBTPTA AMTA WEST`,
+        address: mail,
+      },
+      replyTo: email,
       to: email,
       subject: `Reset your Password: Mail no ${Math.floor(
         Math.random() * 1000 + 1
       )}`,
-      // text: `Your OTP is ${otp}`,
+      text: `Hello Dear ${name}!`,
       html: `<h1 style="text-align:center; color:blue; ">Hello Dear ${name}!</h1>
         <h2 style="text-align:center; color:blue;">Your OTP is ${code}. Please use this OTP to reset your password.</h2>`,
     };
-    const mailResponse = await transport.sendMail(
-      mailOptions,
-      function (error: any, info: any) {
-        if (error) {
-          console.log("error", error);
+
+    await new Promise((resolve, reject) => {
+      // send mail
+      transport.sendMail(mailData, (err: any, info: any) => {
+        if (err) {
+          console.error(err);
+          reject(err);
         } else {
           console.log("Email Sent: " + info.response);
+          resolve(info);
         }
-      }
-    );
-
-    return mailResponse;
+      });
+    });
+    return "Email sent successfully";
   } catch (error) {
     console.log(error);
   }
